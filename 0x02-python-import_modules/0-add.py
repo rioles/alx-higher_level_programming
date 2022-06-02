d94fafe505789574ffc599a6b1ece8183acdf3c4
rioles <rioles1992@gmail.com>
2022-06-02 15:16:17 +0000

    fixe bug

--- 0x02-python-import_modules/0-add.py
+++ 0x02-python-import_modules/0-add.py
@@ -1,7 +1,12 @@
 #!/usr/bin/python3
 from add_0 import add
-if __name__ == "__main__":
+
+def main():
     a = 1
-    b = 3
-    print(add(a,b))
+    b = 2
+    c = add(a, b)
+    print("{} + {} = {}".format(a, b, c))
+
+if __name__ == "__main__":
+    main()
 
