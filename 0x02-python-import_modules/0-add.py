7b0e8fe123bceb6e38e74dfa74c6cb603ecd7f9e
rioles <rioles1992@gmail.com>
2022-06-02 15:34:41 +0000

    fixed bug

--- 0x02-python-import_modules/0-add.py
+++ 0x02-python-import_modules/0-add.py
@@ -1,6 +1,11 @@
 #!/usr/bin/python3
 from add_0 import add
-if __name__ == "__main__":
+
+
+def print_sum():
     a = 1
     b = 2
-    print(f"{a} + {b} = {add(a,b)}")
+    print(f"{a} + {b} = {add(a, b)}")
+
+if __name__ == "__main__":
+    print_sum()
