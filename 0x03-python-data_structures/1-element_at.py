e83ae4ef7d13b1549eefaf912ec1705d6fd0b6f7
rioles <rioles1992@gmail.com>
2022-06-04 08:31:50 +0000

    fixe issue

--- 0x03-python-data_structures/1-element_at.py
+++ 0x03-python-data_structures/1-element_at.py
@@ -15,10 +15,7 @@ def element_at(my_list, idx):
         None if the index is larger than the list length
     """
 
-
-    if idx < 0:
-        return (None)
-    elif idx > (len(my_list) - 1):
-        return (None)
+    if idx < 0 or idx > (len(my_list) - 1):
+        return None
     else:
         return my_list[idx]
