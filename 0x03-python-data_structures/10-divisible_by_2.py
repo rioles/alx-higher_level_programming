7eb2936bbc6f248276db5fa175cb723627e66f0f
rioles <rioles1992@gmail.com>
2022-06-06 10:45:16 +0000

    fixed bug

--- 0x03-python-data_structures/10-divisible_by_2.py
+++ 0x03-python-data_structures/10-divisible_by_2.py
@@ -1,6 +1,22 @@
+#!/usr/bin/python3
 def divisible_by_2(my_list=[]):
+    """
+    Finds all multiples of 2 in a list
+    ...
+    Parameters
+    ----------
+    my_list : list
+        The list to check
+    Return:
+        A new list with True or False, depending
+         on whether the integer at the same position
+         in the original list is a multiple of 2
+    """
+
+
     result = [ False for i in my_list ]
     for i in range(0, len(my_list)):
         if my_list[i] % 2 == 0:
-            result[i] = True     
-    return result
+            result[i] = True
+
+    return (result)
--- 0x03-python-data_structures/9-max_integer.py
+++ 0x03-python-data_structures/9-max_integer.py
@@ -14,10 +14,9 @@ def max_integer(my_list=[]):
     if len(my_list) == 0:
         return (None)
     elif len(my_list) == 1:
-        return (my_list[i])
+        return (my_list[0])
     else:
         for i in range(0, len(my_list)-1):
             if my_list[i] > my_list[i + 1]:
-                my_list[i], my_list[i + 1] = my_list[i + 1], my_list[i]
-            
+                my_list[i], my_list[i + 1] = my_list[i + 1], my_list[i] 
         return (my_list[i + 1])
