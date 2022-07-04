243f9fa1633d1c031302d79761dcd4f0c7b341a8
rioles <rioles1992@gmail.com>
2022-07-04 14:43:04 +0000

    resolved pycodestyle error

--- 0x0A-python-inheritance/0-lookup.py
+++ 0x0A-python-inheritance/0-lookup.py
@@ -1,9 +1,9 @@
 #!/usr/bin/python3
 # 0-lookup.py
-
 """ File name : 0-lookup.py
     It is not allowed to import any module
 """
+
 
 def lookup(obj):
     """Return a list of an object's available attributes
