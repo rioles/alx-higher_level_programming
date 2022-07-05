f596eb84e3fe57cfc34cfb1c2c140e0c88826b69
rioles <rioles1992@gmail.com>
2022-07-05 16:42:12 +0000

    resolved pycodestyle error

--- 0x0B-python-input_output/1-write_file.py
+++ 0x0B-python-input_output/1-write_file.py
@@ -1,6 +1,8 @@
 #!/usr/bin/python3
 # 3-write_file.py
 """Defines a file-writing function."""
+
+
 def write_file(filename="", text=""):
     """Write a string to a UTF8 text file
 
@@ -10,4 +12,3 @@ def write_file(filename="", text=""):
     """
     with open(filename, mode="w", encoding="utf-8") as f:
         return f.write(text)
-
