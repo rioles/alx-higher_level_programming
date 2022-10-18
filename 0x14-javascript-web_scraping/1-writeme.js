debbd0227083e5e9ba0f7a904535490b2eacac1a
rioles <rioles1992@gmail.com>
2022-10-18 18:25:32 +0100

    fix semistandard conflict

--- 0x14-javascript-web_scraping/1-writeme.js
+++ 0x14-javascript-web_scraping/1-writeme.js
@@ -1,8 +1,7 @@
 #!/usr/bin/node
 const fs = require('fs');
-fs.writeFile(process.argv[2],  process.argv[3], err => {
-    if (err) {
-      console.error(err);
-      return;
-    }
-  });
+fs.writeFile(process.argv[2], process.argv[3], err => {
+  if (err) {
+    console.error(err);
+  }
+});
